1752cec74c463443affe08e10f46d4e968f50272
arnabbera <arnabera.work@gmail.com>
2024-04-20 11:06:16 +0200

    added 3 models

--- model.py
+++ model.py
@@ -1,1 +1,7 @@
 print("Adding all ML related codes")
+def model1():
+    print("Adding all ML model1")
+def model2():
+    print("Adding all ML model2")
+def model3():
+    print("Adding all ML model3")
